09551b46cd1cea13486c41d016f31f0595b795c3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-08 15:07:46 +0200

    comment

--- R/Driver.R
+++ R/Driver.R
@@ -52,6 +52,7 @@ setMethod(
 setMethod(
   "dbDataType", "KazamDriver",
   function(dbObj, obj, ...) {
+    # Optional: Can remove this if all data types conform to SQL-92
     tryCatch(
       getMethod("dbDataType", "DBIObject", asNamespace("DBI"))(dbObj, obj, ...),
       error = function(e) testthat::skip("Not yet implemented: dbDataType(Driver)"))
