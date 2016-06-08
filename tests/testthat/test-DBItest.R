06c09c17ddb31f4114638857d27a91a702779e19
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-08 15:07:50 +0200

    named context

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,4 +1,4 @@
-DBItest::make_context(Kazam(), list())
+DBItest::make_context(Kazam(), list(), name = "RKazam")
 DBItest::test_all(c(
   ".*stress.*", # TODO: Enable
   "quote_identifier_not_vectorized",
