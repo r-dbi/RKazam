a76feb93093c56217f15c4146b8d8e159842e7fc
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-11 21:42:17 +0200

    explicit tweaks

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,4 +1,4 @@
-DBItest::make_context(Kazam(), list(), name = "RKazam")
+DBItest::make_context(Kazam(), list(), tweaks = DBItest::tweaks(), name = "RKazam")
 DBItest::test_all(c(
   ".*stress.*", # TODO: Enable
   "quote_identifier_not_vectorized",
