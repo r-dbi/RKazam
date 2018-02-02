eb8b7844aad9e76e4274cb32e9607c4034ed3d59
Kirill Müller <krlmlr+r@mailbox.org>
2018-02-02 16:23:56 +0100

    up tic

--- tic.R
+++ tic.R
@@ -2,10 +2,12 @@ get_stage("after_success") %>%
   add_step(step_hello_world()) %>%
   add_step(step_run_code(covr::codecov()))
 
-if (ci()$get_branch() == "production" && Sys.getenv("BUILD_PKGDOWN") != "") {
+get_stage("deploy") %>%
+  add_step(step_install_ssh_keys()) %>%
+  add_step(step_test_ssh())
+
+if (Sys.getenv("id_rsa") != "" && Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("deploy") %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_test_ssh())
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+    add_step(step_push_deploy())
 }
