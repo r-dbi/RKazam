094cd4e4c1713c33360fca6989b0c1decdf7ff51
Kirill Müller <krlmlr+r@mailbox.org>
2020-12-22 07:24:45 +0100

    - Add comment pointing to implementations that help grow a data frame (#3).
    Closes #3.

--- R/Result.R
+++ R/Result.R
@@ -42,6 +42,15 @@ setMethod(
 setMethod(
   "dbFetch", "KazamResult",
   function(res, n = -1, ...) {
+    #
+    # FOR IMPLEMENTERS:
+    #
+    # If you are interfacing against a native database library,
+    # consider copying and adapting Db*.cpp and Db*.h from r-dbi/RPostgres.
+    # These classes help growing a data frame whose number of rows
+    # may be unknown at the beginning.
+    # Similar code is in r-dbi/odbc.
+    #
     testthat::skip("Not yet implemented: dbFetch(Result)")
   })
 
