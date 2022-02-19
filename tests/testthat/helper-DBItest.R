fbaa7fde8211ec8de67761a59c031f68f0a07747
Kirill Müller <krlmlr+r@mailbox.org>
2022-02-19 05:50:52 +0100

    Revert "Ignore test with older DBItest"
    This reverts commit eed1a4ff8d493b89370eec330cc6c9ae671d0969.

--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -16,7 +16,6 @@ DBItest::make_context(
     # TODO: Understand why test fails in R < 3.6
     if (getRversion() < "3.6") "connect_format",
     # Fails with older DBItest
-    if (packageVersion("DBItest") < "1.7.2") "constructor",
     if (packageVersion("DBItest") < "1.7.2") "reexport",
     NULL
   )
