612ed9487aad9ecddf794636b6debabdc513adeb
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-19 17:23:46 +0200

    Default dbAppendTable()

---
+++ R/dbAppendTable_Connection_character_data.frame.R
@@ -0,0 +1,19 @@
+dbAppendTable_KazamConnection <- function(conn, name, value, ..., row.names = NULL) {
+  # FIXME: Remove when parameterized binding is implemented
+
+  if (!is.null(row.names)) {
+    stop("Can't pass `row.names` to `dbAppendTable()`", call. = FALSE)
+  }
+  stopifnot(is.data.frame(value))
+
+  query <- sqlAppendTable(
+    con = conn,
+    table = name,
+    values = value,
+    row.names = row.names,
+    ...
+  )
+  dbExecute(conn, query)
+}
+#' @export
+setMethod("dbAppendTable", signature("KazamConnection"), dbAppendTable_KazamConnection)
