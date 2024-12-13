78b6f528a4af76df398cfac19f6f567538b68ca7
Kirill Müller <kirill@cynkra.com>
2024-12-13 07:35:17 +0100

    fix: Reexport all generics as expected by DBItest

--- R/Connection.R
+++ R/Connection.R
@@ -29,16 +29,46 @@ DBI::dbUnquoteIdentifier
 DBI::dbGetQuery
 
 #' @export
+DBI::dbGetQueryArrow
+
+#' @export
+DBI::dbFetch
+
+#' @export
+DBI::dbFetchArrowChunk
+
+#' @export
 DBI::dbExecute
 
 #' @export
 DBI::dbReadTable
 
 #' @export
+DBI::dbReadTableArrow
+
+#' @export
+DBI::dbWriteTable
+
+#' @export
+DBI::dbWriteTableArrow
+
+#' @export
 DBI::dbCreateTable
 
 #' @export
+DBI::dbCreateTableArrow
+
+#' @export
 DBI::dbAppendTable
+
+#' @export
+DBI::dbAppendTableArrow
+
+#' @export
+DBI::dbBind
+
+#' @export
+DBI::dbBindArrow
 
 #' @export
 DBI::dbListObjects
